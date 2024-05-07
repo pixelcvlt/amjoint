850369527db94e026db7ee7382d1a264d67aad4d
pixelcvlt <122495751+pixelcvlt@users.noreply.github.com>
2024-05-06 20:03:05 -0700

    Update App.js

--- scripts/App.js
+++ scripts/App.js
@@ -2,7 +2,7 @@ import { useRive } from "rive-react";
 
 function App() {
   const { rive, RiveComponent } = useRive({
-    src: "https://github.com/pixelcvlt/amjoint/raw/master/riv/simple5.riv",
+    src: "/simple5.riv",
     animations: "State Machine 1",
     autoplay: true,
   })
